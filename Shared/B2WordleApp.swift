8c45a35c8b7fc40e6bbbbe2cf767bc76571dac9f
Eduardo Callado <eduardo_tasker@hotmail.com>
2022-02-21 14:42:13 -0300

    Initial commit

---
+++ Shared/B2WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  B2WordleApp.swift
+//  Shared
+//
+//  Created by Eduardo Callado on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct B2WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
